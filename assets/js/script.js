084e856d2a1a9c6a8590ec0154b951e0a0192078
Raenique27 <raeniquewalters@gmail.com>
2022-08-11 15:54:33 -0400

    added function to save content put in text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
+
+var saveTextContent = function(){
+    textContentNine = $("#textContentNine").val();
+    textContentTen = $("#textContentTen").val();
+    textContentEleven = $("#textContentEleven").val();
+    textContentTwelve = $("#textContentTwelve").val();
+    textContentOne = $("#textContentOne").val();
+    textContentTwo = $("#textContentTwo").val();
+    textContentThree = $("#textContentThree").val();
+    textContentFour = $("#textContentFour").val();
+    textContentFive = $("#textContentFive").val();
+};
+
+$(".saveBtn").addEventListener("click", saveTextContent);
+
