b63ec9d94759e43f3e029837e840771542444a74
Raenique27 <raeniquewalters@gmail.com>
2022-08-11 16:27:48 -0400

    added function to hold local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,24 @@ var saveTextContent = function(){
     textContentThree = $("#textContentThree").val();
     textContentFour = $("#textContentFour").val();
     textContentFive = $("#textContentFive").val();
+
+    storeTextContent();
 };
 
 $(".saveBtn").addEventListener("click", saveTextContent);
+
+var storeTextContent = function() {
+    localStorage.setItem("#textContentNine",textContentNine);
+    localStorage.setItem("#textContentTen",textContentTen);
+    localStorage.setItem("#textContentEleven", textContentEleven);
+    localStorage.setItem("#textContentTwelve",textContentTwelve);
+    localStorage.setItem("#textContentOne",textContentOne);
+    localStorage.setItem("#textContentThree",textContentThree);
+    localStorage.setItem("#textContentFour",textContentFour);
+    localStorage.setItem("#textContentFive",textContentFive);
+};
+
+
+
+
 
